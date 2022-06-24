487d275ecafaf2415c461cf88e023a426d4f0f21
kjh6511 <jinhee6511@naver.com>
2022-06-24 17:56:48 +0900

    22.06.24

--- src/main/java/com/spring/study/memo/MemoController.java
+++ src/main/java/com/spring/study/memo/MemoController.java
@@ -10,7 +10,7 @@ public class MemoController {
         String string = "string";
         List<Integer> integerList = new ArrayList<>();
         Integer[] integerArry = {3, 1, 2, 5, 7};
-        Member member = new Member();
+        Member member = new Member(); //인스턴스
         List<Member> list = new ArrayList<>();
         List<Member> listA = new ArrayList<>(Collections.singletonList(member));
 
@@ -18,7 +18,7 @@ public class MemoController {
         System.out.println(string); // 출력
         System.out.println("member" + member); //memberMember [id=null, pw=0]
         System.out.println("list" + integerList); //list[], .isEmpty(null 아님)
-
+        System.out.println("listA" + listA); // listA[Member [id=null, pw=0]]
 
         //문자열 String
         string.startsWith("st"); //st로 시작하는 문자
@@ -36,9 +36,16 @@ public class MemoController {
         integerList.add(2); integerList.add(null); //리스트 값 추가 [2, null]
         integerList.remove(null); //null 지우기 [2]
 
-
         //람다식 Lamda
 
+        //힙: 동적메모리 -> 인스턴스 생성시 할당된다 new로 생성
+        //객체: 객체 지향 프로그램의 대상, 생성된 인스턴스
+        //클래스: 객체를 프로그래밍 하기위해 코드로 정의해 놓은 상태
+        //인스턴스: new 키워드를 사용하여 클래스르 메모리에 생성한 상태
+        //멤버변수 : 클래스의 속성,특징
+        //메서드 : 멤버변수를 이용하여 클래스의 기능을 구현한 함수
+        //참조변수 : 메모리에 생성된 인스턴스르 가리키는 변수
+        //참조값 : 생성된 인스턴스의 메모리 주소값
     }
 
 }
@@ -47,8 +54,8 @@ public class MemoController {
 //getter
 //setter
 class Member{
-    private String id;
-    private int pw;
+    public String id;
+    public int pw;
 
     Member(){}
 
